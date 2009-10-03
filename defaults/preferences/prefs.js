f46deadcbd13011b53257a1fb52995921fbd834e
Eric James Michael Ritz <Eric@cybersprocket.com>
2009-10-03 14:56:49 -0400

    Display Javascript warnings and errors in the console

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,2 +1,6 @@
 // The locatiion of our primary UI, loaded when the application starts.
 pref("toolkit.defaultChromeURI", "chrome://CSSchemer/content/xul/main.xul");
+
+// Show Javascript warnings and errors in the console.
+pref("javascript.options.showInConsole", true);
+pref("javascript.options.strict", true);
