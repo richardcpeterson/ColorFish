ea79a137fe21b945447c54d99718f5b54a20d336
Eric James Michael Ritz <Eric@cybersprocket.com>
2009-10-10 15:10:10 -0400

    Allow using dump() if application is started with '-console'
    Also clarify that the app must be run with '-jsconsole' for the
    Javascript warnings and errors to be shown.

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,6 +1,12 @@
 // The locatiion of our primary UI, loaded when the application starts.
 pref("toolkit.defaultChromeURI", "chrome://CSSchemer/content/xul/main.xul");
 
-// Show Javascript warnings and errors in the console.
+// Show Javascript warnings and errors if the application is started
+// with the '-jsconsole' flag.
 pref("javascript.options.showInConsole", true);
 pref("javascript.options.strict", true);
+
+// Allow using dump() to send out data if application is started
+// with the '-console' flag.
+pref("browser.dom.window.dump.enabled", true);
+
