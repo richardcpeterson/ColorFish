025fcaa7a43bc5346e4a261ece2e383658008ef0
Eric James Michael Ritz <Eric@cybersprocket.com>
2009-10-10 16:42:57 -0400

    Create a Browser class and global instance
    This refactors the code in 'browser.js' into a class, in preparation
    of adding more complex logic around browser interactions.  At the
    expense of invoking the wrath of the gods, I also create a global
    instance of the Browser class.  The way the class is defined, it
    automatically communicates with the embedded <browser> in our XUL; if
    new Browser objects are created they will deal with this same browser.
    So instead of having to write 'new Browser()' all over the place, I
    think it is more straight-forward to simply have a global for the
    Browser.
    
    Note, however, that other Browser objects *could* point to different
    <browser> elements by changing the 'widget' property.  Not sure why
    would ever do so, though.

--- chrome/content/scripts/browser.js
+++ chrome/content/scripts/browser.js
@@ -1,8 +1,39 @@
 /***
- * Contains functions for manipulating the embedded browser.
+ * This file creates a class for interacting with the browser embedded
+ * inside of the application interface.  We also create a global
+ * instance of this class called 'Browser' for all interactions
+ * outside of this file.
  */
 
-function load_page() {
-    document.getElementById("browser")
-        .loadURI( document.getElementById("url-bar").value );
+function Browser() {
+
+    /***
+     * The <browser> element inside our XUL document.  Because we go
+     * ahead and call getElementById() here we need to make sure that
+     * this file is not included via <script> until after the
+     * <browser> element we want to use for our source.
+     */
+    this.widget = document.getElementById("browser");
+
+    /***
+     * Takes a URL as a string and loads that page.
+     */
+    this.load_page = function(url) {
+        this.widget.loadURI(url);
+    }
+
+    /***
+     * Loads the URL in the address bar.
+     */
+    this.load_from_address_bar = function() {
+        this.load_page(
+            document.getElementById("url-bar").value
+        );
+    }
+
 }
+
+/***
+ * Our global browser instance.
+ */
+var Browser = new Browser();
