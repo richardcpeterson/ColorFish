e89058c99d7a2108bf7ec538b7841248d5005a37
U-CSL-CYBERPOWER1\Richard <Richard@csl-cyberpower1.(none)>
2009-10-21 17:29:31 -0400

    Added browser back, forward and refresh buttons - not complete
    This is still buggy, and needs the broadcasters refactored out into the commands

--- chrome/content/scripts/browser.js
+++ chrome/content/scripts/browser.js
@@ -14,7 +14,13 @@ function Browser() {
      * <browser> element we want to use for our source.
      */
     this.widget = document.getElementById("browser");
-
+    this.widget.addProgressListener(new browserListener(),0xFFFFFFFF);
+    
+    /**
+     * Hold reference to the address bar input
+     */
+    this.address_bar = document.getElementById("uri-input");
+    
     /***
      * Takes a URL as a string and loads that page.
      */
@@ -27,8 +33,22 @@ function Browser() {
      */
     this.load_from_address_bar = function() {
         this.load_page(
-            document.getElementById("uri-input").value
+            this.address_bar.value
         );
+    }
+    
+    this.reload = function() {
+        this.widget.reload();
+    }
+    
+    this.forward = function() {
+        if(this.widget.canGoForward)
+            this.widget.goForward();
+    }
+    
+    this.back = function() {
+        if (this.widget.canGoBack)
+            this.widget.goBack();
     }
 
     /***
@@ -38,10 +58,54 @@ function Browser() {
     this.get_stylesheets = function() {
         return this.widget.contentDocument.styleSheets;
     }
-
+    
+    this.update_address_bar = function() {
+        this.address_bar.value = this.widget.currentURI.spec;
+    }
+    
+    this.update_back_forward_commands = function() {
+        var backBroadcaster = document.getElementById("canGoBack");
+        var forwardBroadcaster = document.getElementById("canGoForward");
+        backBroadcaster.setAttribute("disabled", !this.widget.canGoBack);
+        forwardBroadcaster.setAttribute("disabled", !this.widget.canGoForward);
+    }
 }
 
-/***
- * Our global browser instance.
+
+/**
+ * Define a browser status listener that implements
+ * nsIWebProgressListener.  This will let us handle
+ * events in the browser.
  */
-var Browser = new Browser();
+function browserListener () {
+    this.count=0;
+
+    this.onStateChange = function(aWebProgress, aRequest, aStateFlags, aStatus) {},
+    this.onStatusChange = function(aWebProgress, aRequest, aStatus, aMessage) {},
+    this.onProgressChange = function(aWebProgress, aRequest, aCurSelfProgress,
+                          aMaxSelfProgress, aCurTotalProgress, aMaxTotalProgress) {},
+    this.onSecurityChange = function(aWebProgress, aRequest, state) {},
+    this.onLocationChange = function(aWebProgress, aRequest, aLocation)
+    {
+        this.count++;
+        alert("browserListener.onLocationChange(): " + this.count);
+        Browser.update_back_forward_commands();
+        Browser.update_address_bar();
+    },
+    
+    this.QueryInterface = function(aIID)
+    {
+        if (aIID.equals(Components.interfaces.nsIWebProgressListener) ||
+            aIID.equals(Components.interfaces.nsISupportsWeakReference) ||
+            aIID.equals(Components.interfaces.nsIXULBrowserWindow) ||
+            aIID.equals(Components.interfaces.nsISupports))
+            return this;
+        throw Components.results.NS_NOINTERFACE;
+    },
+    this.setJSStatus = function(status) {},
+    this.setJSDefaultStatus = function(status) {},
+    this.setOverLink = function(link) {}
+}
+
+//Declare a global browser, to be initialized later
+var Browser;
--- chrome/content/scripts/init.js
+++ chrome/content/scripts/init.js
@@ -3,7 +3,29 @@
  */
 
 function initApp(){
+  /***
+   * Our global browser instance.
+   */
+  window.Browser = new Browser();
+  
   addHandlerToElement("uri-input", "keypress", loadPageOnEnterKey);
+}
+
+function dumpProps(obj, parent) {
+   // Go through all the properties of the passed-in object
+   for (var i in obj) {
+      // if a parent (2nd parameter) was passed in, then use that to
+      // build the message. Message includes i (the object's property name)
+      // then the object's property value on a new line
+      if (parent) { var msg = parent + "." + i + "\n" + obj[i]; } else { var msg = i + "\n" + obj[i]; }
+      // Display the message. If the user clicks "OK", then continue. If they
+      // click "CANCEL" then quit this level of recursion
+      if (!confirm(msg)) { return; }
+      // If this property (i) is an object, then recursively process the object
+      if (typeof obj[i] == "object") {
+         if (parent) { dumpProps(obj[i], parent + "." + i); } else { dumpProps(obj[i], i); }
+      }
+   }
 }
 
 
