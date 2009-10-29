b721b8a856ce700dfa7221068917ac6da415e95f
Richard Peterson <richard@cybersprocket.com>
2009-10-29 10:39:05 -0400

    Create new Page class
    Class to model a page, with all its state, palettes, etc

---
+++ chrome/content/scripts/page.js
@@ -0,0 +1,22 @@
+/***
+ * Class for maintaining information
+ * about and modifying a browsed webpage. This should
+ * not be confused with the browser, which renders
+ * pages and navigates between them. 
+ */
+
+function Page(contentDocument) {
+    this.document = null;
+    this.originalPalette = null;
+    this.resultPalette = null;
+    this.styleSheets = null;
+    
+    if (contentDocument){
+        this.document = contentDocument;
+        this.originalPalette = new Palette(this.document);
+        this.resultPalette = new Palette(this.document);
+        //this.resultPalette = originalPalette.clone();
+        this.styleSheets = this.document.styleSheets;
+    }
+}
+
