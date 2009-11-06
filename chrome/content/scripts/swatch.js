625446a8d1a6b64913adf4c84fe2d4c5e0ea849f
Eric James Michael Ritz <Eric@cybersprocket.com>
2009-11-06 04:19:31 -0500

    Refactor comparisons of hue and lightness in Swatch class
    By introducing a generic comparison function---compareNumbers()---we
    can reduce the need for any temporary variables and nested conditions
    within the compareHue() and compareHueAndLightness() functions.

--- chrome/content/scripts/swatch.js
+++ chrome/content/scripts/swatch.js
@@ -5,10 +5,10 @@
 
 function Swatch(color){
     this.color = color;
-    
+
     //An array of color properties
     this.properties = new Array();
-    
+
     /**
      * Update all properties in this swatch to reflect
      * the value in this.color
@@ -19,14 +19,14 @@ function Swatch(color){
             this.properties[i].setColor(hexColor);
         }
     }
-    
+
     /**
      * Add a color property to this swatch
      */
     this.addProperty = function(newProperty){
         this.properties.push(newProperty);
     }
-    
+
     /**
      * Return number of references in this swatch
      */
@@ -35,16 +35,34 @@ function Swatch(color){
     }
 }
 
+/***
+ * Takes two numbers.  The return value is:
+ *
+ *   1  if  x > y
+ *  -1  if  x < y
+ *   0  if  x = y
+ *
+ * This makes it more convenient to write functions below for
+ * comparing hue and lightness.
+ */
+Swatch.compareNumbers = function(x, y) {
+    return (
+        (x > y) ? 1 : (x < y) ? -1 : 0
+    );
+}
+
+
 /**
  * Compare the hue of one swatch to another.
  * if hue(s1)>hue(s2), returns positive.
  * if hue(s1)<hue(s2), returns negative.
  * if hue(s1)==hue(s2), returns 0
  */
-Swatch.compareHue = function(s1, s2){
-    var s1hue = s1.color.getHue();
-    var s2hue = s2.color.getHue();
-    return ((s1hue<s2hue)?-1:((s1hue>s2hue)?1:0));
+Swatch.compareHue = function(s1, s2) {
+    return Swatch.compareNumbers(
+        s1.color.getHue(),
+        s2.color.getHue()
+    );
 }
 
 /**
@@ -55,25 +73,9 @@ Swatch.compareHue = function(s1, s2){
  *    lightness by the same rules.
  * if hue and lightness are equal, than returns 0.
  */
-Swatch.compareHueAndLightness = function(s1, s2){
-    var s1hue = s1.color.getHue();
-    var s2hue = s2.color.getHue();
-    var s1lightness = s1.color.getLightness();
-    var s2lightness = s2.color.getLightness();
-    return (
-        //Here's a fun one for you...
-        (s1hue<s2hue)? //if
-            -1
-        :( //else
-            (s1hue>s2hue)? //if
-                1
-            :( //else
-                (s1lightness<s2lightness)? //if
-                    -1
-                :( //else
-                    (s1lightness>s2lightness)?1:0
-                )
-            )
-        )
+Swatch.compareHueAndLightness = function(s1, s2) {
+    return Swatch.compareHue(s1, s2) || Swatch.compareNumbers(
+        s1.color.getLightness(),
+        s2.color.getLightness()
     );
 }
