53bab02bf040cffcc714c8e1f071eb8e296af158
Richard Peterson <richard@cybersprocket.com>
2009-11-06 09:30:47 -0500

    Make Page read its list of stylesheets from the Palette, not the Document
    The document only contains top-level parent stylesheets, not any imported
    child stylesheets. The Palette contains all processed stylesheets. The page
    is using ALL stylesheets, not just top-level stylesheets, so we need to
    reflect that in the Page class (which propogates to the UI...)

--- chrome/content/scripts/page.js
+++ chrome/content/scripts/page.js
@@ -14,9 +14,9 @@ function Page(contentDocument) {
     if (contentDocument){
         this.document = contentDocument;
         this.originalPalette = new Palette(this.document);
+        this.styleSheets = this.originalPalette.styleSheets;
         //this.resultPalette = new Palette(this.document);
         //this.resultPalette = originalPalette.clone();
-        this.styleSheets = this.document.styleSheets;
     }
 }
 
