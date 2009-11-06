89268a4cd183a6593a3f2404cc6136aa364688f1
Richard Peterson <richard@cybersprocket.com>
2009-11-06 11:03:43 -0500

    Add methods for manipulating CSS / DOM classes of Elements
    Adds the addClass(), hasClass() and removeClass() methods to
    all Elements, allowing you to manipulate the classes without having
    to maniupulate the class string. This stuff should be standard in the
    DOM by now! ... but it isn't. So we add it.

--- chrome/content/scripts/init.js
+++ chrome/content/scripts/init.js
@@ -14,6 +14,10 @@ function initApp(){
         return new Array(count + 1).join(this);
     }
     
+    /**
+     * Allow arrays to tell us if they contain
+     * a particular element as a value
+     */
     Array.prototype.contains = function(item){
         var found = false;
         var i = 0;
@@ -22,5 +26,33 @@ function initApp(){
             i++;
         }
         return found;
+    }
+    
+    /**
+     * Check if the element has a particular class
+     * assigned to it in its class string
+     */
+    Element.prototype.hasClass = function(classString){
+        return this.className.match(new RegExp("\\b" + classString + "\\b"));
+    }
+    
+    /**
+     * Add a class to an element's class string
+     * (if it isn't already in the string)
+     */
+    Element.prototype.addClass = function(classString) {
+        if (!this.hasClass(classString))
+            this.className += " "+classString;
+    }
+    
+    /**
+     * Remove a class from an element, if it exists
+     * in the element's class string
+     */
+    Element.prototype.removeClass = function(classString) {
+        if (this.hasClass(classString)) {
+            var reg = new RegExp("\\b" + classString + "\\b");
+            this.className = this.className.replace(reg,' ');
+        }
     }
 }
