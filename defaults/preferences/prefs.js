21bd8893bd6373a41e293d58c112d7c18c7c4f34
Richard Peterson <richard@cybersprocket.com>
2010-01-14 11:16:04 -0500

    Add development preferences - no caching of XUL, etc

--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,12 +1,27 @@
-// The locatiion of our primary UI, loaded when the application starts.
+/*************************************
+ * GENERAL SETTINGS
+ ************************************/
+// The location of our primary UI, loaded when the application starts.
 pref("toolkit.defaultChromeURI", "chrome://CSSchemer/content/xul/main.xul");
+
+
+
+/*************************************
+ *  DEVELOPMENT SETTINGS
+ ************************************/
+//TODO - these are development settings. We either need
+//To do some sort of conditional build or remember to
+//manually take these out
 
 // Show Javascript warnings and errors if the application is started
 // with the '-jsconsole' flag.
 pref("javascript.options.showInConsole", true);
 pref("javascript.options.strict", true);
 
+pref("nglayout.debug.disable_xul_cache", true);
+pref("nglayout.debug.disable_xul_fastload", true);
+
 // Allow using dump() to send out data if application is started
 // with the '-console' flag.
 pref("browser.dom.window.dump.enabled", true);
-
+pref("browser.cache.disk.enable", false);
