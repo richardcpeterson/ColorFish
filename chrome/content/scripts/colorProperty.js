c56d6984f647abd525436f1c0fd17c28cb9771c5
Eric James Michael Ritz <Eric@cybersprocket.com>
2010-03-12 11:00:46 -0500

    ColorProperty: Redefine functions on the prototype for performance
    Since it is costly to define these functions every time we instantiate
    ColorProperty objects, we take out the functions and define them on
    the prototype instead, so that we are not re-creating them every time.
    Some of the functions originally referred to 'propertyType' and were
    changed to refer to 'this.propertyType', since we no longer close over
    the value passed into the constructor.

--- chrome/content/scripts/colorProperty.js
+++ chrome/content/scripts/colorProperty.js
@@ -17,33 +17,25 @@
 function ColorProperty(style, propertyType) {
     this.style = style;
     this.propertyType = propertyType;
-    this.originalFormat = 0;
-
-    //Functions to be defined based on propertyType
-    this.setColor = null;
-    this.getColor = null;
-
-    // Define these functions depending on which propertyType this
-    // ColorProperty is supposed to access.
-    this.setColor = function(colorString) {
-        this.style.update(propertyType, colorString);
-    };
-
-    this.undoColor = function(colorString) {
-        return this.style.undo();
-    };
-
-    this.getColorString = function() {
-        return this.style.property(propertyType);
-    };
-
-    /**
-     * Return a new Color object representing this
-     * property's color
-     */
-    this.getColor = function(){
-        return Color.from_css(this.getColorString());
-    }
-
     this.originalFormat = Color.getFormat(this.getColorString());
 }
+
+ColorProperty.prototype.setColor = function (colorString) {
+    this.style.update(this.propertyType, colorString);
+};
+
+ColorProperty.prototype.getColorString = function () {
+    return this.style.property(this.propertyType);
+};
+
+ColorProperty.prototype.undoColor = function (colorString) {
+    return this.style.undo();
+};
+
+/**
+ * Return a new Color object representing this property's color.
+ */
+ColorProperty.prototype.getColor = function () {
+    return Color.from_css(this.getColorString());
+}
+
